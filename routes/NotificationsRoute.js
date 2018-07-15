d88ee4edc91777865c8999cde839601db53469a3
receiptdev <receipt.defy@me.com>
2018-07-15 16:31:19 +0900

    Building the Navigation

---
+++ routes/NotificationsRoute.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from "react-navigation";
+import NotificationsScreen from "../screens/NotificationsScreen";
+import sharedRoutes, { sharedOptions } from "./sharedRoutes";
+
+const NotificationsRoute = createStackNavigator(
+    {
+        Home: {
+            screen: NotificationsScreen
+        },
+        ...sharedRoutes
+    },
+    { ...sharedOptions }
+);
+
+export default NotificationsRoute;
---
+++ routes/SearchRoute.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from "react-navigation";
+import SearchScreen from "../screens/SearchScreen";
+import sharedRoutes, { sharedOptions } from "./sharedRoutes";
+
+const SearchRoute = createStackNavigator(
+    {
+        Home: {
+            screen: SearchScreen
+        },
+        ...sharedRoutes
+    },
+    { ...sharedOptions }
+);
+
+export default SearchRoute;
